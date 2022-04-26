c095d921a24bfadb713196710bf3378c0d331e74
Aditya <adityataware0709@gmail.com>
2022-04-26 23:09:20 +0530

    Adding some extras in vector stl

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 void display(vector<int> v)
@@ -45,6 +46,43 @@ int main(){
     vector<int> :: iterator iter = v1.begin();
     v1.insert(iter + 2,100);
     display(v1);
+
+    // Sorting vector
+    vector <int> v = {21, 4, 16, 19, 2};
+    cout<<"Before sorting array is ";
+    display(v);
+    sort(v.begin(), v.end());     // Time complexity = O(nlog(n))
+    cout<<"After sorting array becomes ";
+    display(v);
+
+    // Binary search for finding 21 in the array
+    bool present = binary_search(v.begin(), v.end(), 21); // true
+    cout<<"Is 21 present in the array: "<<endl;
+    cout<<present;
+
+    present = binary_search(v.begin(), v.end(), 210); // false
+    cout<<"Is 210 present in the array: "<<endl;
+    cout<<present;
+
+
+    v.push_back(100);
+    v.push_back(100);
+    v.push_back(100);
+
+    // For finding the first ocurrence of a letter 
+    vector<int>::iterator it1 = lower_bound(v.begin(), v.end(), 100); // Returns position of element less than or equal to 100 also keep in mind it will return first ocurrence
+    cout<<"First pos of 100 in array ";
+    display(v);
+    cout<<"is "<<*it1<<endl;
+
+    vector<int>::iterator it2 = upper_bound(v.begin(), v.end(), 100);// Returns pos of first element greater 100 
+
+    // To find the count of 100 in an array
+    int count = it2 - it1;
+    cout<<"Number of ocurrences of 100 in the array ";
+    display(v);
+    cout<<count<<endl;
+
 
     return 0;
 }
